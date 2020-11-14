3a20007c849f05ee246c7f6ed42207423bd8df5d
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-11-14 20:10:10 +0000

    sbin/create_certs.sh: Create node certificates

--- sbin/create_certs.sh
+++ sbin/create_certs.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # create certificates for everyone
 # usage: sbin/create_certs.sh path/prefix
@@ -8,25 +8,52 @@
 #
 # will create certificates in dir ./certs with the prefix 'dev'
 # (dev-ca.key, dev-server.key, etc.)
+set -e # exit on first error
 
+HOST_DOMAIN_SUFFIX="${HOST_DOMAIN_SUFFIX:-.local}"
+CA_HOSTNAME="${CA_HOSTNAME:-ca}"
 
-if [ $# -ne 1 ]
+if [ $# -lt 2 ]
 then
-    echo "ERROR: need one positional argument (prefix)"
-    echo "usage: sbin/create_certs.sh certs/dev"
+    echo "ERROR: need at least two positional arguments (prefix)"
+    echo "Usage: $0 <prefix> <hostname>*"
+    echo "HOST_DOMAIN_SUFFIX: $HOST_DOMAIN_SUFFIX"
+    echo "CA hostname: $CA_HOSTNAME"
     exit -1
 fi
 
 prefix=$1
+shift 1
 
-# generate CA
-openssl genrsa -out $prefix-ca.key 2048
-openssl req -x509 -new -nodes -key $prefix-ca.key -sha256 -days 1825 -out $prefix-ca.crt
+openssl_subj() {
+    echo "/C=PT/ST=Lisboa/L=Lisboa/O=Universidade de Lisboa/OU=Instituto Superior Tecnico, SIRS G41/CN=${1}.local/"
+}
 
-# generate server key and CSR
-openssl genrsa -out $prefix-server.key 2048
-openssl req -new -key $prefix-server.key -out $prefix-server.csr -config resources/certs.conf
+openssl_cert_opts() {
+    echo "-days 365 -sha3-512"
+}
 
-exit 0
-# sign key
-openssl x509 -req -in $prefix-server.csr -CA $prefix-ca.crt -CAkey $prefix-ca.key -CAcreateserial -out $prefix-server.crt -days 10000
+gen_privkey() {
+    # Usage: gen_privkey <hostname>
+    openssl genrsa -out "$prefix${1}.key" 3072
+}
+
+gen_cert() {
+    # Usage: gen_cert <hostname>
+    openssl req -new -key "$prefix${1}.key" -subj "$(openssl_subj ${1})" -out "$prefix${1}.csr"
+    openssl x509 -req -in "$prefix${1}.csr" -CA "$prefix${CA_HOSTNAME}.pem" -CAkey "$prefix${CA_HOSTNAME}.key" -CAcreateserial -out "$prefix${1}.pem" $(openssl_cert_opts)
+    rm "$prefix${1}.csr"
+}
+
+echo "Creating CA"
+gen_privkey "$CA_HOSTNAME"
+# create self-signed certificate for CA
+openssl req -x509 -new -nodes -key "$prefix$CA_HOSTNAME.key" $(openssl_cert_opts) -subj "$(openssl_subj "${CA_HOSTNAME}")" -out "$prefix${CA_HOSTNAME}.pem"
+echo
+
+for host in $@; do
+    echo "Creating key/cert for $host"
+    gen_privkey "$host"
+    gen_cert "$host"
+    echo
+done
