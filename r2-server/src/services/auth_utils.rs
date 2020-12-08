91f7d5a4985c9de34327d57cfbd7f0d645f3c04a
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-12-08 17:11:46 +0000

    r2-server: rustfmt

--- r2-server/src/services/auth_utils.rs
+++ r2-server/src/services/auth_utils.rs
@@ -1,12 +1,13 @@
 use openssl::x509::X509;
-use openssl_utils::X509Ext;
-use std::sync::{RwLock, Arc};
+use openssl_utils::PublicKeyFingerprintExt;
+use std::sync::{Arc, RwLock};
 use tokio::task;
-use tonic::{Request, Status, transport::Certificate};
+use tonic::{transport::Certificate, Request, Status};
 
 pub async fn authenticate<T>(req: &Request<T>) -> Result<Vec<u8>, Status> {
-    let certs_ref = req.peer_certs_global()
-        .ok_or(Status::internal("can't authenticate client: no global certs object (broken implementation)"))?;
+    let certs_ref = req.peer_certs_global().ok_or(Status::internal(
+        "can't authenticate client: no global certs object (broken implementation)",
+    ))?;
 
     let mut id = try_auth(certs_ref.clone());
     let mut remaining_attempts = 1000i32;
@@ -20,27 +21,28 @@ pub async fn authenticate<T>(req: &Request<T>) -> Result<Vec<u8>, Status> {
         }
     }
 
-    id
-        .ok_or(Status::internal("can't authenticate client: maximum attempts reached"))?
+    id.ok_or(Status::internal(
+        "can't authenticate client: maximum attempts reached",
+    ))?
 }
 
 fn try_auth(certs_lock: Arc<RwLock<Option<Vec<Certificate>>>>) -> Option<Result<Vec<u8>, Status>> {
-    let certs_guard = certs_lock
-        .read()
-        .expect("poisoned lock");
+    let certs_guard = certs_lock.read().expect("poisoned lock");
 
-    certs_guard
-        .as_ref()
-        .map(|certs| {
-            // the first certificate is the client's certificate (lowest in chain)
-            certs.first()
+    certs_guard.as_ref().map(|certs| {
+        // the first certificate is the client's certificate (lowest in chain)
+        certs
+            .first()
             .ok_or(Status::internal("can't authenticate client: no certs"))
             .and_then(|client_cert| {
                 X509::from_der(client_cert.as_ref())
                     .map_err(|err| err.into())
                     .and_then(|c| c.pubkey_fingerprint())
-                    .map_err(|_| Status::internal("can't authenticate client: unable to parse cert fingerprint"))
+                    .map_err(|_| {
+                        Status::internal(
+                            "can't authenticate client: unable to parse cert fingerprint",
+                        )
+                    })
             })
-        })
-
+    })
 }
