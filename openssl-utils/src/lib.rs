b517ef74d18fb1172e7ceed987d7caeb3b9faeb2
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-12-10 19:03:18 +0000

    openssl-utils: Simplify errors

--- openssl-utils/src/lib.rs
+++ openssl-utils/src/lib.rs
@@ -1,5 +1,5 @@
 use openssl::error::ErrorStack;
-use std::fmt;
+use thiserror::Error;
 
 pub mod pubkey_fingerprint;
 pub use pubkey_fingerprint::PublicKeyFingerprintExt;
@@ -24,41 +24,32 @@ pub enum KeyUsage {
     NonRepudiation,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum CryptoErr {
-    OpensslError(ErrorStack),
+    #[error("OpenSSL Error: {:?}", .0)]
+    OpensslError(#[from] ErrorStack),
+    
+    #[error("Certificate missing Common Name field")]
     NoCommonName,
+
+    #[error("Certificate has too many common name fields")]
     TooManyCommonNames,
+
+    #[error("Invalid Certificate")]
     InvalidCert,
+
+    #[error("Incompatible key usage")]
     IncompatibleKeyUsage,
+
+    #[error("Certificate expired")]
     Expired,
+
+    #[error("Message too big")]
     MessageTooBig,
+
+    #[error("Bad key size")]
     BadKeySize,
+
+    #[error("Invalid signature")]
     InvalidSignature,
 }
-
-impl From<ErrorStack> for CryptoErr {
-    fn from(err: ErrorStack) -> Self {
-        CryptoErr::OpensslError(err)
-    }
-}
-
-impl fmt::Display for CryptoErr {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            CryptoErr::Expired => write!(f, "Certificate Expired"),
-            CryptoErr::IncompatibleKeyUsage => write!(f, "Incompatible key usage"),
-            CryptoErr::InvalidCert => write!(f, "Invalid Certificate"),
-            CryptoErr::MessageTooBig => write!(f, "Certificate Expired"),
-            CryptoErr::NoCommonName => write!(f, "Certificate missing Common Name field"),
-            CryptoErr::TooManyCommonNames => {
-                write!(f, "Too many common name fields in certificate")
-            }
-            CryptoErr::BadKeySize => write!(f, "Bad Key Size"),
-            CryptoErr::InvalidSignature => write!(f, "Signature is invalid"),
-            CryptoErr::OpensslError(e) => write!(f, "OpenSSL error: {}", e),
-        }
-    }
-}
-
-impl std::error::Error for CryptoErr {}
