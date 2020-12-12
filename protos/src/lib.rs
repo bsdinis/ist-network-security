2a1302f345c1dc470b9f24cc39d368b2c4ad416d
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-12-12 08:11:48 +0000

    protos: Implement Serialize for protobuf messages

--- protos/src/lib.rs
+++ protos/src/lib.rs
@@ -1,5 +1,7 @@
 // We have streams, streams have non camel case type names :(
 #![allow(non_camel_case_types)]
 
+use serde::Serialize; // used in include_proto!
+
 tonic::include_proto!("r2");
 tonic::include_proto!("r2_identity");
